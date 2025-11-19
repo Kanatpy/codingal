44f6fc16262e0ddb8d4c9b57eb2687ead1e46e84
Kanatpy <ekinkanatmarangoz@gmail.com>
2025-11-18 18:18:01 -0800

    Add files via upload

---
+++ type casting.py
@@ -0,0 +1,16 @@
+name="penguin"
+age=15
+is_student=True
+weight=38.5
+print("name :", name)
+print("data type of name is", type(name))
+print("age:", age)
+print("data type of age is", type(age))
+print("student :", is_student)
+print("data type of student is", type(is_student))
+print("weight :", weight)
+print("data type of weight is", type(weight))
+age=str(age)
+print("data type of age is", type(age))
+name = name [::-1]
+print(name)
