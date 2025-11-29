34af9090986fe804d05061f23e3512defff5e1cb
Kanatpy <ekinkanatmarangoz@gmail.com>
2025-11-28 18:28:29 -0800

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,56 @@
+print("write something")
+x = str(input())
+if x == ("a"):
+  print("you WIN")
+elif x == ("b"):
+  print("you WIN")
+elif x == ("c"):
+  print("you WIN")
+elif x == ("d"):
+  print("you WIN")
+elif x == ("e"):
+  print("you WIN")
+elif x == ("f"):
+  print("you WIN")
+elif x == ("g"):
+  print("you WIN")
+elif x == ("h"):
+  print("you WIN")
+elif x == ("i"):
+  print("you WIN")
+elif x == ("j"):
+  print("you WIN")
+elif x == ("k"):
+  print("you WIN")
+elif x == ("l"):
+  print("you WIN")
+elif x == ("m"):
+  print("you WIN")
+elif x == ("n"):
+  print("you WIN")
+elif x == ("o"):
+  print("you WIN")
+elif x == ("p"):
+  print("you WIN")
+elif x == ("q"):
+  print("you WIN")
+elif x == ("r"):
+  print("you WIN")
+elif x == ("s"):
+  print("you WIN")
+elif x == ("t"):
+  print("you WIN")
+elif x == ("u"):
+  print("you WIN")
+elif x == ("v"):
+  print("you WIN")
+elif x == ("w"):
+  print("you WIN")
+elif x == ("x"):
+  print("you WIN")
+elif x == ("y"):
+  print("you WIN")
+elif x == ("z"):
+  print("you WIN")
+else:
+  print("you LOSE")
