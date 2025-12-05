f1d7743fd1bd6df71c6b871390b91c1226ba1806
Kanatpy <ekinkanatmarangoz@gmail.com>
2025-12-04 19:35:22 -0800

    Add files via upload

---
+++ mean.py
@@ -0,0 +1,10 @@
+x=38
+y=36
+z=56
+w=40
+sum = x*w
+print("the sum of 40 numbers is: ",sum)
+num_2=sum+((z)-(y))
+print("sum - ((wrong numbers)-(correct numbers)): ",num_2)
+mean2=num_2/w
+print(mean2)
