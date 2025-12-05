50ade7e86236145cb5af4a050769677e96a7f917
Kanatpy <ekinkanatmarangoz@gmail.com>
2025-12-04 19:20:56 -0800

    Add files via upload

---
+++ presidents.py
@@ -0,0 +1,15 @@
+v = 4
+w = 5
+x = 8
+y =2
+z = 0
+z = (v + w) * x / y;
+print("value of (v+w) * x/y is ", z)
+
+name = "alex"
+age = 0
+#and can be changed with or to be "hello welcome"
+if name =="alex" and name == "jhon" and age >2:
+    print("hello welcome")
+else:
+    print("goodbye")
