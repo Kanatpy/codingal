4b2a3b23e075eb94a2c7e2e1a9f7ae63b55d4ec5
Kanatpy <ekinkanatmarangoz@gmail.com>
2025-12-16 19:42:39 -0800

    Add files via upload

---
+++ reverse a string.py
@@ -0,0 +1,6 @@
+x = input("please enter your own string: ")
+y = ('')
+for i in x:
+    y = i + y
+print("\nThe original string = ",x)
+print("\nThe reversed string string = ",y)
---
+++ sum of whole numbers.py
@@ -0,0 +1,5 @@
+x = int(input("enter a num whose sum you want to find: "))
+sum=0
+for i in range(1, x+1):
+    sum= sum + i
+    print("\nSum =", sum)
