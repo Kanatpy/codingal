776d54cc09fc4e39d3d49df4acbd0faa9b2ae288
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-01-08 20:48:16 -0800

    Refactor triangle printing logic and input method
    Refactor left angle triangle printing logic and update input handling.

--- left angle triangle.py
+++ left angle triangle.py
@@ -1,17 +1,13 @@
 x = int(input("enter the num of rows: "))
-if x %2==0:
-    y = int(x/2)
-else:
-    y = int(x/2)+1
-z = y-1
-for i in range(1,y+1):
+z = x-1
+for i in range(1,x+1):
     for j in range(1,z+1):
         print(end=" ")
     z = z-1
     a =1
-#            if does not work replace  [for j in range(i-1)]  with  [for j in range(2*i-1)]  to get back to rormal
-    for j in range(i-1):
-        print(end=str(a))
+#            if does not work replace  [for j in range(2*i-1)]  with  [for j in range(2*i-1)]  to get back to rormal
+    for j in range(1,i+1):
+        print(end="*")
         a = a+1
     print()
 z =1
