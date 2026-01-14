e0aac9e3abcd9fd58dbecac58af5f935eed42aff
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-01-13 20:36:58 -0800

    Add files via upload

---
+++ break.py
@@ -0,0 +1,7 @@
+word = input("enter a word: ")
+for i in word:
+    if (i == "t" or i =="T"):
+        print("T is found")
+        
+    else:
+        print("T not found")
---
+++ continu.py
@@ -0,0 +1,7 @@
+var = int(input("enter a num: "))
+while var >0:
+    var = var - 1
+    if var == 5:
+        continue
+    print("\ncurrent var value =",var)
+print("\ngood bye!")
