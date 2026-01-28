846ecdca1291695547d0638d53f90f531f988361
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-01-27 18:12:40 -0800

    Add files via upload

---
+++ math stuff.py
@@ -0,0 +1,11 @@
+import math
+print("f,c of 23.56 = " + str(math.ceil(23.56)) + str(math.floor(23.56)))
+
+x =10
+y = -15
+print(str(math.copysign(x, y)))
+print(str(math.fabs(-96)) + str(math.fabs(56)))
+print(str(math.gcd(24,56)))
+print("y exp is"str(math.exp(y)))
+print("x degree is"str(math.degree(x)))
+print("")
