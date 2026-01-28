103715a3bc48099a86fcca0036938eb518edd2e1
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-01-27 18:16:11 -0800

    Add files via upload

---
+++ due amount.py
@@ -0,0 +1,10 @@
+def bill(a,b):
+    return a-b
+    
+
+print("milk is 15.35 you need to pay")
+payed =float(input("how much: "))
+milk =15.35
+print("milk is 15.35 you need to pay")
+money=bill(payed,milk)
+print(money)
