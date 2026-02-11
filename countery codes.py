d8cc9555eeb98906af8484db35256d13a3beea2d
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-02-10 20:39:36 -0800

    Add files via upload

---
+++ countery codes.py
@@ -0,0 +1,12 @@
+countrys = {"India":"0091",
+            "Autralia":"0025",
+            "Turkey":"0097"}
+
+print("Country code for India -")
+print(countrys.get("India","Not fount ERROR"))
+
+print("Country code for Canada -")
+print(countrys.get("Canada","Not fount ERROR"))
+
+print("Country code for Turkey -")
+print(countrys.get("Turkey","Not fount ERROR"))
