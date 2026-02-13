1ee545bb69878ea0ba3d523f37811962c5bf3c15
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-02-12 20:10:39 -0800

    Update pr of tuplles.py

--- pr of tuplles.py
+++ pr of tuplles.py
@@ -1,11 +1,10 @@
 num_of_tuples = int(input("Enter the number of tuples: "))
 num = 0
 tup1 =() 
+product = 1
 for i in range(num_of_tuples):
     tuple_input = input("Enter your first,etc: ")
     tup1 = tup1 + (tuple_input,)
-while num <= -1:
-    tuple_elements1 = tup1[num]
-    num +=1
-    
-print("multipul of your nums combinded is", tup1)
+for a in range(len(tup1)):
+    product = product * int(tup1[a])
+print("multipul of your nums combinded is", product)
