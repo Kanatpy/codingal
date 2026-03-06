a8c2114733d746263f34686e3a57b7eb020b9c4f
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-03-05 19:49:42 -0800

    Update a + p of a circle.py

--- a + p of a circle.py
+++ a + p of a circle.py
@@ -1,9 +1,14 @@
 class circle:
     pi = 3.14159
     def __init__(self, radius):
-        r = float(radius)
-        self.area = self.pi * (r ** 2)
-        self.curcumference = (r * 2) * self.pi
+        self.r = float(radius)
+        
+    def a(self):
+        self.area = self.pi * (self.r ** 2)
+        return self.area
+    def p(self):
+        self.curcumference = (self.r * 2) * self.pi
+        return self.curcumference
 radius = input("radius: ")
 circ1 = circle(radius)
-print(f"\n  area:{circ1.area} , curcumference:{circ1.curcumference}")
+print(f"\n  area:{circ1.a()} , curcumference:{circ1.p()}")
