1bb55f7372fcf4b6a6116289f98796a1529ab7e5
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-04-16 18:31:08 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -34,7 +34,7 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_of_enemies = 6
+num_of_enemies = 8
 
 for i in range(num_of_enemies):
     enemyImg.append(pygame.transform.scale_by(pygame.image.load("enemie.png"), 4))
