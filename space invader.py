b358f079f06350a75c1f1e20cc10fbad70ddfd91
Kanatpy <ekinkanatmarangoz@gmail.com>
2026-04-16 19:57:15 -0700

    Update space invader.py

--- space invader.py
+++ space invader.py
@@ -3,10 +3,8 @@ import pygame
 import random
 import sys
 
-
 screen_width = 800
 screen_height = 500
-
 player_start_x = 370
 player_start_y = 380
 enemy_start_y_min = 50
@@ -17,11 +15,20 @@ bullet_vel = 10
 collision_distance = 27
 
 pygame.init()
+pygame.mixer.init()
+
+pygame.mixer.music.load("backround music.mp3")
+pygame.mixer.music.set_volume(0.07)
+pygame.mixer.music.play(-1)
+
+shootSFX = pygame.mixer.Sound("shoot.mp3")
+explosionSFX = pygame.mixer.Sound("explosion.mp3")
+GAMEOVERSFX = pygame.mixer.Sound("GAMEOVER.mp3")
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Space Game")
 
-background = pygame.transform.scale(pygame.image.load("backround.png"), (screen_width,screen_height))
+background = pygame.transform.scale(pygame.image.load("backround.png"), (screen_width, screen_height))
 pygame.display.set_icon(pygame.image.load("icon.png"))
 
 player_img = pygame.transform.scale_by(pygame.image.load("player.png"), 4)
@@ -34,7 +41,7 @@ enemyX = []
 enemyY = []
 enemyX_change = []
 enemyY_change = []
-num_of_enemies = 8
+num_of_enemies = 7
 
 for i in range(num_of_enemies):
     enemyImg.append(pygame.transform.scale_by(pygame.image.load("enemie.png"), 4))
@@ -56,8 +63,9 @@ def show_da_score():
     screen.blit(score, (10, 10))
 
 def GAMEOVER():
-    over_text = game_font.render("GAME OVER", True, (255, 0,0))
+    over_text = game_font.render("GAME OVER", True, (255, 0, 0))
     screen.blit(over_text, (350, 200))
+    GAMEOVERSFX.play(1)
 
 def player(x, y):
     screen.blit(player_img, (x, y))
@@ -65,24 +73,18 @@ def player(x, y):
 def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
 
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
+def draw_bullet(x, y):
     screen.blit(bulletImg, (x + 16, y + 10))
 
-def isCollision(enemyX_val, enemyY_val, bulletX_val, bulletY_val):
-    distance = math.sqrt((enemyX_val - bulletX_val) ** 2 + (enemyY_val - bulletY_val) ** 2)
+def isCollision(ex, ey, bx, by):
+    distance = math.sqrt((ex - bx) ** 2 + (ey - by) ** 2)
     return distance < collision_distance
 
 clock = pygame.time.Clock()
 
-
-
 while True:
     clock.tick(60)
     screen.blit(background, (0, 0))
-
-
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -95,8 +97,9 @@ while True:
             if event.key == pygame.K_d:
                 playerX_change = 5
             if event.key == pygame.K_SPACE and bullet_state == "ready":
+                shootSFX.play()
                 bulletX = playerX
-                fire_bullet(bulletX, bulletY)
+                bullet_state = "fire"
 
         if event.type == pygame.KEYUP:
             if event.key in [pygame.K_a, pygame.K_d]:
@@ -110,28 +113,28 @@ while True:
             for j in range(num_of_enemies):
                 enemyY[j] = 2000
             GAMEOVER()
-            pygame.display.update()
-            continue
+            break
 
         enemyX[i] += enemyX_change[i]
-
         if enemyX[i] <= 0 or enemyX[i] >= screen_width - enemyImg[i].get_width():
             enemyX_change[i] *= -1
             enemyY[i] += enemyY_change[i]
 
-        if isCollision(enemyX[i], enemyY[i], bulletX, bulletY):
-            bulletY = player_start_y
-            bullet_state = "ready"
-            score_value += 1
-            enemyX[i] = random.randint(0, screen_width - enemyImg[i].get_width())
-            enemyY[i] = random.randint(enemy_start_y_min, enemy_start_y_max)
-            enemy_vel_x += 3
-            enemy_vel_y += 5
+        if bullet_state == "fire":
+            if isCollision(enemyX[i], enemyY[i], bulletX, bulletY):
+                bulletY = player_start_y
+                bullet_state = "ready"
+                score_value += 1
+                enemyX[i] = random.randint(0, screen_width - 60)
+                enemyY[i] = random.randint(enemy_start_y_min, enemy_start_y_max)
+                enemy_vel_x += 3
+                enemy_vel_y += 5
+                explosionSFX.play()
 
         enemy(enemyX[i], enemyY[i], i)
 
     if bullet_state == "fire":
-        fire_bullet(bulletX, bulletY)
+        draw_bullet(bulletX, bulletY)
         bulletY -= bullet_vel
         if bulletY <= 0:
             bulletY = player_start_y
